b364194cf13e570844107d75dc2df157adfd77eb
shounakG <shounak.gujarathi@gmail.com>
2015-11-30 21:24:08 -0800

    AlchemyAPI code

---
+++ alchemyapi-recipes-twitter/credentials.py
@@ -0,0 +1,3 @@
+twitter_consumer_key='qL8TJLZ5OdRwhFk0sl6S18nXu' 
+twitter_consumer_secret='CXsdyMb0n8g0NyriNmeDrrwC5h4NKBzYqTYcicpxjGQX03dP8B'
+alchemy_apikey='28c7e9bc4f7933940b8632440910197386b3b3ba'
